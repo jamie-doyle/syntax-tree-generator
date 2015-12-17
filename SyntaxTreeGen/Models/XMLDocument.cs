aa0d76ffeaa6e5a3b0af46f2563ffd5ee9b3a747
Jamie Doyle <jamie.doyle@me.com>
2015-12-17 07:44:38 +0000

    Adding XML doc class

---
+++ SyntaxTreeGen/Models/XMLDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxTreeGen.Models
+{
+    /// <summary>
+    /// An XML document
+    /// </summary>
+    class XmlDocument
+    {
+
+    }
+}
