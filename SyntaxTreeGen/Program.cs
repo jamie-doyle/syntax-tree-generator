ce0642b3d244610f63afa3074e57140650bfb5e7
Jamie Doyle <jamie.doyle@me.com>
2016-04-18 18:01:27 +0100

    Adding parser wrapper (Document.cs) to source control. "Hello world" example as parsable XML (HelloWorld.xml)

--- SyntaxTreeGen/Program.cs
+++ SyntaxTreeGen/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Xml;
-using SyntaxTreeGen.Models;
 using SyntaxTreeGen.XML;
-using static SyntaxTreeGen.Models.OperatorNode;
 
 namespace SyntaxTreeGen
 {
@@ -10,64 +7,17 @@ namespace SyntaxTreeGen
     {
         static void Main(string[] args)
         {
-            const string path = @"C:\Users\Jamie\Source\Repos\Syntax Tree\SyntaxTreeGen\Test.xml";
-            
-            var doc = new Document(path);
+            const string testPath = @"C:\Users\Jamie\Source\Repos\Syntax Tree\SyntaxTreeGen\XML\Examples\";
+            const string helloWorld = @"HelloWorld.xml";
+           
+            var doc = new Document(testPath + helloWorld);
+
             var docnodes = doc.GetNodes();
+
             Console.WriteLine(docnodes);
             Console.ReadLine();
 
             //TODO: Generate tutorial programs from node representation (Deitel & Deitel)
-
-            ////try
-            ////{
-            ////    var doc = new Document(path);
-            ////    Console.ReadLine();
-            ////}
-            ////catch (XmlException ex)
-            ////{
-            ////    Console.WriteLine("This document could not be read as " + ex.Message + ".");
-            ////    if (ex.LineNumber > 0)
-            ////        Console.WriteLine("The error ocurred on line "+ex.LineNumber+", position "+ex.LinePosition);
-            ////}
-
-            //// Class
-            //var testClass = new ClassNode(
-            //    ClassNode.AccessLevel.Public, false, "FooClass",
-            //    // First method
-            //    new MethodNode("Capitalise", true, MethodNode.AccessLevel.Private, "void",
-            //        // Parameters
-            //        new Node[]
-            //        {
-            //            new VarNode("int", "x")
-            //        },
-            //        // Statements
-            //        new Node[]
-            //        {
-            //            // assign ten -> x
-            //            new AssignNode(new VarNode("int", "ten"), new ConstantNode(10)),
-
-            //            // assing (x ^ 2) -> res
-            //            new AssignNode(
-            //                new VarNode("int", "res"), 
-            //                new OperatorNode(new VarNode("int", "res"), OpKind.ToPowerOf, new ConstantNode(2)) 
-            //                )
-            //        }
-            //    ) // END METHOD
-            //);
-
-
-
-            //var assignResX = new AssignNode( new VarNode("int", "res"), new VarNode("int", "x"));
-
-            //var innerIf = new IfNode(new ConstantNode(true), assignResX);
-
-            ////var ifMethod = new MethodNode("IfStatement", false, MethodNode.ProtectionLevelKind.Public, )
-
-            //var res = testClass.ToString();
-
-            //Console.WriteLine(res);
-            //Console.ReadLine();
         }
     }
 }
---
+++ SyntaxTreeGen/XML/Document.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using SyntaxTreeGen.Models;
+using SyntaxTreeGen.XML.Parsers;
+
+namespace SyntaxTreeGen.XML
+{
+    /// <summary>
+    /// An XML document
+    /// </summary>
+    public class Document
+    {
+        private readonly string _docPath;
+
+        public Document(string path)
+        {
+            _docPath = path;
+
+            // check XML doc is valid
+            _docPath = Path.GetFullPath(_docPath);
+
+            if (Path.GetExtension(_docPath).ToLower() != ".xml")
+                throw new ArgumentException("The file \"" + _docPath + "\" is not an XML document.");
+
+            // Try loading the XML doc to check it's valid
+            try
+            {
+                var doc = new XmlDocument();
+                doc.Load(_docPath);
+            }
+            catch (XmlException)
+            {
+                throw new XmlException("The given file's XML is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Get the ClassNode and its child nodes, as represented by this Document
+        /// </summary>
+        /// <returns>Top node (e.g., ClassNode) generated from the XML</returns>
+        public ClassNode GetNodes()
+        {
+            // head stores the parsed class and its children
+            ClassNode head = null;
+
+            // Create a new reader, configured to ignore comments, whitespace and directives 
+            var settings = new XmlReaderSettings
+            {
+                IgnoreWhitespace = true,
+                IgnoreComments = true,
+                IgnoreProcessingInstructions = true
+            };
+            
+            using (var reader = XmlReader.Create(_docPath, settings))
+            {
+                // Move reader to outer class statement 
+                reader.Read();
+
+                // Do parsing
+                if (reader.Name.ToLower().Trim() == "class")
+                {
+                    var parser = new ClassParser(reader);
+                    head = (ClassNode) parser.Result;
+                }
+                else
+                {
+                    throw new ArgumentException("The given XML file doesn't contain a class");
+                }
+            }
+
+            return head;
+        }
+    }
+}
